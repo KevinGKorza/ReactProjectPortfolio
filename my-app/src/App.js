af8f0191e4d9b3c0d16d9ba3a59720cec8c51ded
kevinkorza <kevingkorza@gmail.com>
2022-11-22 09:21:47 -0500

    navbar update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,11 @@
 import "../src/style.css";
+import Navbar from "./components/Navbar";
 
 
 export default function App(){
   return (
     <div className="App">
-      <h1>Hello and WELCOME to my Portfolio</h1>
-      <h2> I hope that you enjoy it!</h2>
-      <h3> It' going to be really fun!</h3>
+      <Navbar/>
     </div>
   );
 }
